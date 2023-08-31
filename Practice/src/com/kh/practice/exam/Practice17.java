4cd36b9a5fe1a178a8a4acc48301ed1511b087f9
jonghwan <whdghks9241@naver.com>
2023-09-01 01:06:09 +0900

    과제 18번 문제 생성

---
+++ Practice/src/com/kh/practice/exam/Practice17.java
@@ -0,0 +1,19 @@
+package com.kh.practice.exam;
+
+public class Practice17 {
+
+	public static void main(String[] args) {
+		/**
+		 * 1부터 사용자에게 입력 받은 수까지 중에서
+			1) 2와 3의 배수를 모두 출력하고
+			2) 2와 3의 공배수의 개수를 출력하세요.
+			* ‘공배수’는 둘 이상의 수의 공통인 배수라는 뜻으로 어떤 수를 해당 수들로 나눴을 때 모두 나머지가 0이 나오는 수
+		 */
+		
+		
+		
+		
+		
+	}
+
+}
