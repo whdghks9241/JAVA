aa3122b8b466546fd0b2a6704a13cdd50b9f1213
JongHwan <42468874+whdghks9241@users.noreply.github.com>
2023-09-21 17:16:41 +0900

    TCP를 이용한 서버통신

--- chap34_Network/src/com/kh/TCPMain/TCPClient.java
+++ chap34_Network/src/com/kh/TCPMain/TCPClient.java
@@ -2,10 +2,14 @@ package com.kh.TCPMain;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class TCPClient {
 
@@ -13,22 +17,26 @@ public class TCPClient {
 		try {
 			//서버 주소와 포트 번호로 소켓 생성
 			//서버 주소와 포트번호는 내가 연결하고자 하는 포트와 동일해야함
-			Socket socket = new Socket("localhost", 810);
+			Socket socket = new Socket("localhost", 12345);
+			System.out.println("Server Connect");
 			
-			PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			
-			// 서버 되는지 테스트 메세지
-			out.println("안녕 서버?");
+			// 보낼때 
+			OutputStream outStream = socket.getOutputStream();
+			PrintStream printStream = new PrintStream(outStream);
 			
-			//서버로부터 메세지 전달되는지 확인 테스트
-			String response = in.readLine();
-			System.out.println("서버 응답 : " + response);
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
+			printStream.print("qweqwe");
+			printStream.flush();
+			
+			// 서버로부터 읽음
+			InputStream InStream = socket.getInputStream();
+			Scanner sc = new Scanner(InStream);
+			System.out.println("[Client] server : " + sc.nextLine());
+			
+			socket.close();
+			System.out.println("Byte Server See you later!! ");
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 
