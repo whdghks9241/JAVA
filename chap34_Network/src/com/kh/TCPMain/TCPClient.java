f86a831209d15a3577cdf80a566680ceced6b0a7
JongHwan <42468874+whdghks9241@users.noreply.github.com>
2023-09-22 14:21:11 +0900

    socket 주소 추가

--- chap34_Network/src/com/kh/TCPMain/TCPClient.java
+++ chap34_Network/src/com/kh/TCPMain/TCPClient.java
@@ -17,7 +17,8 @@ public class TCPClient {
 		try {
 			//서버 주소와 포트 번호로 소켓 생성
 			//서버 주소와 포트번호는 내가 연결하고자 하는 포트와 동일해야함
-			Socket socket = new Socket("localhost", 12345);
+//			Socket socket = new Socket("1.221.120.194", 5200);
+			Socket socket = new Socket("localhost", 5200);
 			System.out.println("Server Connect");
 			
 			
